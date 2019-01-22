a6ce039949b8b90d1666ef5c93a6cdd1439dc33c
Sergej Jovanovic <sergej@gnedo.com>
2019-01-22 15:30:50 +0100

    Made changes based on comments for PRs: https://github.com/webrtc-uwp/chromium-build/pull/3 https://github.com/webrtc-uwp/chromium-build/pull/4

--- vs_toolchain.py
+++ vs_toolchain.py
@@ -190,12 +190,12 @@ def _CopyUCRTRuntime(target_dir, source_dir, target_cpu, dll_pattern, suffix):
       os.environ.get('WINDOWSSDKDIR',
                      os.path.expandvars('%ProgramFiles(x86)%'
                                         '\\Windows Kits\\10')))
-  if target_cpu != 'arm64':
-    ucrt_dll_dirs = os.path.join(win_sdk_dir, 'Redist', 'ucrt', 'DLLs',
-                                 target_cpu)
-  else:
+  if target_cpu == 'arm64':
     ucrt_dll_dirs = os.path.join(win_sdk_dir, 'Redist', 'ucrt', 'DLLs',
                                  'arm')
+  else:
+    ucrt_dll_dirs = os.path.join(win_sdk_dir, 'Redist', 'ucrt', 'DLLs',
+                                 target_cpu)
   ucrt_files = glob.glob(os.path.join(ucrt_dll_dirs, 'api-ms-win-*.dll'))
   assert len(ucrt_files) > 0
   for ucrt_src_file in ucrt_files:
@@ -244,6 +244,7 @@ def _CopyPGORuntime(target_dir, target_cpu):
     pgo_x86_runtime_dir = os.path.join(pgo_runtime_root, 'HostX86', 'x86')
     pgo_x64_runtime_dir = os.path.join(pgo_runtime_root, 'HostX64', 'x64')
     pgo_arm_runtime_dir = os.path.join(pgo_runtime_root, 'arm')
+    pgo_arm64_runtime_dir = os.path.join(pgo_runtime_root, 'arm64')
   else:
     raise Exception('Unexpected toolchain version: %s.' % env_version)
 
@@ -257,7 +258,9 @@ def _CopyPGORuntime(target_dir, target_cpu):
     elif target_cpu == 'x64':
       source = os.path.join(pgo_x64_runtime_dir, runtime)
     elif target_cpu == 'arm':
-      source = os.path.join(pgo_arm_runtime_dir, runtime)	  
+      source = os.path.join(pgo_arm_runtime_dir, runtime)	 
+    elif target_cpu == 'arm64':
+      source = os.path.join(pgo_arm64_runtime_dir, runtime)	       
     else:
       raise NotImplementedError("Unexpected target_cpu value: " + target_cpu)
     if not os.path.exists(source):
