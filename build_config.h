4d0d324c44080fdc1d142f6ba20313fd448ab6e5
Sergej Jovanovic <sergej@gnedo.com>
2019-08-05 22:55:56 +0200

    Resolve arm unit test build issues

--- build_config.h
+++ build_config.h
@@ -135,7 +135,7 @@
 #define ARCH_CPU_PPC64 1
 #define ARCH_CPU_64_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
-#elif defined(__ARMEL__)
+#elif defined(__ARMEL__) || defined(_M_ARM)
 #define ARCH_CPU_ARM_FAMILY 1
 #define ARCH_CPU_ARMEL 1
 #define ARCH_CPU_32_BITS 1
